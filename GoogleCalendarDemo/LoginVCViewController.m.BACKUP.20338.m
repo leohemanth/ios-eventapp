fc4773f05f756080847fd041c864c917d7d5caac
bhatshreenidhi <bhatshreenidhi@gmail.com>
2014-04-05 11:27:30 -0700

    merge

---
+++ GoogleCalendarDemo/LoginVCViewController.m.BACKUP.20338.m
@@ -0,0 +1,201 @@
+//
+//  LoginVCViewController.m
+//  USCEvents
+//
+//  Created by Hemanth on 04/04/14.
+//  Copyright (c) 2014 Andrew Davis. All rights reserved.
+//
+
+#import "LoginVCViewController.h"
+#import "Event.h"
+# import <FacebookSDK/FacebookSDK.h>
+#import "ADManagedObjectContext.h"
+#import <Parse/Parse.h>
+#import "ADCalendarViewController.h"
+@interface LoginVCViewController ()
+
+@end
+
+@implementation LoginVCViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    FBLoginView *loginView = [[FBLoginView alloc] initWithReadPermissions:@[@"basic_info",@"email"]];
+    // Align the button in the center horizontally
+    loginView.delegate = self;
+    
+    loginView.frame = CGRectOffset(loginView.frame, (self.view.center.x - (loginView.frame.size.width / 2)),95);
+    [self.view addSubview:loginView];
+    // Do any additional setup after loading the view.
+}
+
+// This method will be called when the user information has been fetched
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
+                            user:(id<FBGraphUser>)user {
+    [self requestEvents];
+    [PFFacebookUtils logInWithPermissions:@[@"basic_info",@"email"] block:^(PFUser *user, NSError *error) {
+        if (!user) {
+            NSLog(@"Uh oh. The user cancelled the Facebook login.");
+        } else if (user.isNew) {
+            NSLog(@"User signed up and logged in through Facebook!");
+        } else {
+            NSLog(@"User logged in through Facebook!");
+        }
+    }];
+}
+
+// Implement the loginViewShowingLoggedInUser: delegate method to modify your app's UI for a logged-in user experience
+- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView {
+    // self.statusLabel.text = @"You're logged in as";
+    
+}
+
+// Implement the loginViewShowingLoggedOutUser: delegate method to modify your app's UI for a logged-out user experience
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView {
+    
+}
+
+// You need to override loginView:handleError in order to handle possible errors that can occur during login
+- (void)loginView:(FBLoginView *)loginView handleError:(NSError *)error {
+    NSString *alertMessage, *alertTitle;
+    
+    // If the user should perform an action outside of you app to recover,
+    // the SDK will provide a message for the user, you just need to surface it.
+    // This conveniently handles cases like Facebook password change or unverified Facebook accounts.
+    if ([FBErrorUtility shouldNotifyUserForError:error]) {
+        alertTitle = @"Facebook error";
+        alertMessage = [FBErrorUtility userMessageForError:error];
+        
+        // This code will handle session closures since that happen outside of the app.
+        // You can take a look at our error handling guide to know more about it
+        // https://developers.facebook.com/docs/ios/errors
+    } else if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryAuthenticationReopenSession) {
+        alertTitle = @"Session Error";
+        alertMessage = @"Your current session is no longer valid. Please log in again.";
+        
+        // If the user has cancelled a login, we will do nothing.
+        // You can also choose to show the user a message if cancelling login will result in
+        // the user not being able to complete a task they had initiated in your app
+        // (like accessing FB-stored information or posting to Facebook)
+    } else if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryUserCancelled) {
+        NSLog(@"user cancelled login");
+        
+        // For simplicity, this sample handles other errors with a generic message
+        // You can checkout our error handling guide for more detailed information
+        // https://developers.facebook.com/docs/ios/errors
+    } else {
+        alertTitle  = @"Something went wrong";
+        alertMessage = @"Please try again later.";
+        NSLog(@"Unexpected error:%@", error);
+    }
+    
+    if (alertMessage) {
+        [[[UIAlertView alloc] initWithTitle:alertTitle
+                                    message:alertMessage
+                                   delegate:nil
+                          cancelButtonTitle:@"OK"
+                          otherButtonTitles:nil] show];
+    }
+}
+
+- (void)requestEvents
+{
+    // We will request the user's events
+    // These are the permissions we need:
+    NSArray *permissionsNeeded = @[@"user_events",@"friends_events",@"friends_about_me",@"friends_location",@"friends_groups",@"user_friends",@"user_groups"];
+    
+    // Request the permissions the user currently has
+    [FBRequestConnection startWithGraphPath:@"/me/permissions"
+                          completionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+                              if (!error){
+                                  NSDictionary *currentPermissions= [(NSArray *)[result data] objectAtIndex:0];
+                                  NSMutableArray *requestPermissions = [[NSMutableArray alloc] initWithArray:@[]];
+                                  
+                                  // Check if all the permissions we need are present in the user's current permissions
+                                  // If they are not present add them to the permissions to be requested
+                                  for (NSString *permission in permissionsNeeded){
+                                      if (![currentPermissions objectForKey:permission]){
+                                          [requestPermissions addObject:permission];
+                                      }
+                                  }
+                                  
+                                  // If we have permissions to request
+                                  if ([requestPermissions count] > 0){
+                                      // Ask for the missing permissions
+                                      [FBSession.activeSession requestNewReadPermissions:requestPermissions
+                                                                       completionHandler:^(FBSession *session, NSError *error) {
+                                                                           if (!error) {
+                                                                               // Permission granted
+                                                                               //NSLog([NSString stringWithFormat:@"new permissions %@", [FBSession.activeSession permissions]]);
+                                                                               // We can request the user information
+                                                                             //  [self fqlRequest];
+<<<<<<< HEAD
+                                                                             //  [self.navigationController pushViewController:[[ADCalendarViewController alloc] init] animated:YES];
+=======
+                                                                        //       [self.navigationController pushViewController:[[ADCalendarViewController alloc] init] animated:YES];
+>>>>>>> f307e7a2f27ba55572c3edf7f537458836eefa86
+                                                                           } else {
+                                                                               // An error occurred, we need to handle the error
+                                                                               // Check out our error handling guide: https://developers.facebook.com/docs/ios/errors/
+                                                                               //                                           NSLog([NSString stringWithFormat:@"error %@", error.description]);
+                                                                           }
+                                                                       }];
+                                  } else {
+                                      // Permissions are present
+                                      // We can request the user information
+                                     // [self fqlRequest];
+                                      [self.navigationController pushViewController:[[ADCalendarViewController alloc] init] animated:NO];
+                                  }
+                                  
+                              } else {
+                                  // An error occurred, we need to handle the error
+                                  // Check out our error handling guide: https://developers.facebook.com/docs/ios/errors/
+                                  //NSLog([NSString stringWithFormat:@"error %@", error.description]);
+                              }
+                          }];
+}
+
+- (void)makeRequestForUserEvents
+{
+    [FBRequestConnection startWithGraphPath:@"me/events?fields=cover,name,start_time"
+                          completionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+                              if (!error) {
+                                  // Success! Include your code to handle the results here
+                                  //  NSLog([NSString stringWithFormat:@"user events: %@", result]);
+                              } else {
+                                  // An error occurred, we need to handle the error
+                                  // Check out our error handling guide: https://developers.facebook.com/docs/ios/errors/
+                                  //   NSLog([NSString stringWithFormat:@"error %@", error.description]);
+                              }
+                          }];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
