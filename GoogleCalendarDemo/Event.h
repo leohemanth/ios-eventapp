7364d770f8b3545196810f6edc6a7d931809ceca
Hemanth <leohemanth@gmail.com>
2014-04-05 01:49:19 -0700

    merge

--- GoogleCalendarDemo/Event.h
+++ GoogleCalendarDemo/Event.h
@@ -18,10 +18,6 @@
 @property (nonatomic, retain) NSString * googleid;
 @property (nonatomic, retain) NSString * location;
 @property (nonatomic, retain) NSString * summary;
-<<<<<<< HEAD
 @property (nonatomic, retain) NSString * fblink;
-
-=======
 @property (nonatomic,retain) NSString * fbid;
->>>>>>> c39cb7c1e3d6fe4093fa84c42be3f04932a7ad04
 @end
